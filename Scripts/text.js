9ba9ff74a63d70b124aa7c125653aa1989050333
Frode Petterson <frode.petterson@amendor.com>
2013-04-13 17:20:11 +0200

    Renamed cpImage and cpText. Added video and audio with flash fallback.

---
+++ Scripts/text.js
@@ -0,0 +1,22 @@
+var H5P = H5P || {};
+
+/**
+ * Constructor.
+ * 
+ * @param {object} params Options for this library.
+ * @param {string} contentPath The path to our content folder.
+ * @returns {undefined}
+ */
+H5P.Text = function (params, contentPath) {
+  this.text = params.text;
+};
+
+/**
+ * Wipe out the content of the wrapper and put our HTML in it.
+ * 
+ * @param {jQuery} $wrapper
+ * @returns {undefined}
+ */
+H5P.Text.prototype.attach = function ($wrapper) {
+  $wrapper.html(this.text);
+};
